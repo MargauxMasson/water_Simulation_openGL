82e3705b400fba5f672e9317e9803b863e64953b
Margaux <margaux.masson21@gmail.com>
2018-11-30 02:01:34 -0800

    Add Freeze action on the waves animation

--- water_simulation.cpp
+++ water_simulation.cpp
@@ -25,7 +25,7 @@
 float Time;
 // #include "sphere.cpp"
 #include "noise.h"
-#define		RESOLUTION 64
+#define		RESOLUTION 100
 #define		MOD	0xff
 
 // title of these windows:
@@ -160,6 +160,7 @@ int width = 1024;
 int height = 512;
 bool isTexture = true;
 int idTexture = 0;
+bool Frozen = false;
 
 unsigned char *Texture1 = BmpToTexture("mercury.bmp", &width, &height);
 unsigned char *Texture2 = BmpToTexture("worldtex.bmp", &width, &height);
@@ -167,21 +168,6 @@ unsigned char *Texture3 = BmpToTexture("mars.bmp", &width, &height);
 unsigned char *Texture = Texture1;
 
 /////////// WATER ////////////
-// static GLuint	texture;
-
-// static int	left_click = GLUT_UP;
-// static int	right_click = GLUT_UP;
-// static int	wire_frame = 0;
-// static int	normals = 0;
-// static int	xold = 0;
-// static int	yold = 0;
-// static float	rotate_x = 30;
-// static float	rotate_y = 15;
-// static float	translate_z = 4;
-
-// static float	surface[6 * RESOLUTION * (RESOLUTION + 1)];
-// static float	normal[6 * RESOLUTION * (RESOLUTION + 1)];
-
 static GLuint	texture;
 
 static int	left_click = GLUT_UP;
@@ -196,8 +182,6 @@ static float	translate_z = 4;
 
 static float	surface[6 * RESOLUTION * (RESOLUTION + 1)];
 static float	normal[6 * RESOLUTION * (RESOLUTION + 1)];
-
-
 
   const float t = glutGet (GLUT_ELAPSED_TIME) / 1000.;
   const float delta = 2. / RESOLUTION;
@@ -237,7 +221,7 @@ static float	normal[6 * RESOLUTION * (RESOLUTION + 1)];
   float l;
 
 
-static float	z (const float x, const float y, const float t)
+static float z (const float x, const float y, const float t)
 {
   const float x2 = x - 3;
   const float y2 = y + 1;
@@ -246,7 +230,6 @@ static float	z (const float x, const float y, const float t)
   return ((2 * sinf (20 * sqrtf (xx + yy) - 4 * t) +
 	   Noise (10 * x, 10 * y, t, 0)) / 200);
 }
-
 
 /*
 ** Function to load a Jpeg file.
@@ -293,247 +276,6 @@ int		load_texture (const char * filename,
   jpeg_destroy_decompress (&cinfo);
   return 0;
 }
-
-// static int		permut[256];
-// static const char	gradient[32][4] =
-//   {
-//     { 1,  1,  1,  0}, { 1,  1,  0,  1}, { 1,  0,  1,  1}, { 0,  1,  1,  1},
-//     { 1,  1, -1,  0}, { 1,  1,  0, -1}, { 1,  0,  1, -1}, { 0,  1,  1, -1},
-//     { 1, -1,  1,  0}, { 1, -1,  0,  1}, { 1,  0, -1,  1}, { 0,  1, -1,  1},
-//     { 1, -1, -1,  0}, { 1, -1,  0, -1}, { 1,  0, -1, -1}, { 0,  1, -1, -1},
-//     {-1,  1,  1,  0}, {-1,  1,  0,  1}, {-1,  0,  1,  1}, { 0, -1,  1,  1},
-//     {-1,  1, -1,  0}, {-1,  1,  0, -1}, {-1,  0,  1, -1}, { 0, -1,  1, -1},
-//     {-1, -1,  1,  0}, {-1, -1,  0,  1}, {-1,  0, -1,  1}, { 0, -1, -1,  1},
-//     {-1, -1, -1,  0}, {-1, -1,  0, -1}, {-1,  0, -1, -1}, { 0, -1, -1, -1},
-// };
-
-
-// void		InitNoise (void)
-// {
-//   unsigned int i = 0;
-//   while (i < 256)
-//     permut[i++] = rand () & MOD;
-// }
-
-// /*
-// ** Function finding out the gradient corresponding to the coordinates
-// */
-// static int	Indice (const int i,
-// 			const int j,
-// 			const int k,
-// 			const int l)
-// {
-//   return (permut[(l + permut[(k + permut[(j + permut[i & MOD])
-// 					 & MOD])
-// 			     & MOD])
-// 		 & MOD]
-// 	  & 0x1f);
-// }
-
-// /*
-// ** Functions computing the dot product of the vector and the gradient
-// */
-// static float	Prod (const float a, const char b)
-// {
-//   if (b > 0)
-//     return a;
-//   if (b < 0)
-//     return -a;
-//   return 0;
-// }
-
-// static float	Dot_prod (const float x1, const char x2,
-// 			  const float y1, const char y2,
-// 			  const float z1, const char z2,
-// 			  const float t1, const char t2)
-// {
-//   return (Prod (x1, x2) + Prod (y1, y2) + Prod (z1, z2) + Prod (t1, t2));
-// }
-
-// /*
-// ** Functions computing interpolations
-// */
-// static float	Spline5 (const float state)
-// {
-//   /*
-//   ** Enhanced spline :
-//   ** (3x^2 + 2x^3) is not as good as (6x^5 - 15x^4 + 10x^3)
-//   */
-//   const float sqr = state * state;
-//   return state * sqr * (6 * sqr - 15 * state + 10);
-// }
-
-// static float	Linear (const float start,
-// 			const float end,
-// 			const float state)
-// {
-//   return start + (end - start) * state;
-// }
-
-// /*
-// ** Noise function, returning the Perlin Noise at a given point
-// */
-// float	Noise (const float x,
-// 		       const float y,
-// 		       const float z,
-// 		       const float t)
-// {
-//   /* The unit hypercube containing the point */
-//   const int x1 = (int) (x > 0 ? x : x - 1);
-//   const int y1 = (int) (y > 0 ? y : y - 1);
-//   const int z1 = (int) (z > 0 ? z : z - 1);
-//   const int t1 = (int) (t > 0 ? t : t - 1);
-//   const int x2 = x1 + 1;
-//   const int y2 = y1 + 1;
-//   const int z2 = z1 + 1;
-//   const int t2 = t1 + 1;
-
-//   /* The 16 corresponding gradients */
-//   const char * g0000 = gradient[Indice (x1, y1, z1, t1)];
-//   const char * g0001 = gradient[Indice (x1, y1, z1, t2)];
-//   const char * g0010 = gradient[Indice (x1, y1, z2, t1)];
-//   const char * g0011 = gradient[Indice (x1, y1, z2, t2)];
-//   const char * g0100 = gradient[Indice (x1, y2, z1, t1)];
-//   const char * g0101 = gradient[Indice (x1, y2, z1, t2)];
-//   const char * g0110 = gradient[Indice (x1, y2, z2, t1)];
-//   const char * g0111 = gradient[Indice (x1, y2, z2, t2)];
-//   const char * g1000 = gradient[Indice (x2, y1, z1, t1)];
-//   const char * g1001 = gradient[Indice (x2, y1, z1, t2)];
-//   const char * g1010 = gradient[Indice (x2, y1, z2, t1)];
-//   const char * g1011 = gradient[Indice (x2, y1, z2, t2)];
-//   const char * g1100 = gradient[Indice (x2, y2, z1, t1)];
-//   const char * g1101 = gradient[Indice (x2, y2, z1, t2)];
-//   const char * g1110 = gradient[Indice (x2, y2, z2, t1)];
-//   const char * g1111 = gradient[Indice (x2, y2, z2, t2)];
-
-//   /* The 16 vectors */
-//   const float dx1 = x - x1;
-//   const float dx2 = x - x2;
-//   const float dy1 = y - y1;
-//   const float dy2 = y - y2;
-//   const float dz1 = z - z1;
-//   const float dz2 = z - z2;
-//   const float dt1 = t - t1;
-//   const float dt2 = t - t2;
-
-//   /* The 16 dot products */
-//   const float b0000 = Dot_prod(dx1, g0000[0], dy1, g0000[1],
-// 			       dz1, g0000[2], dt1, g0000[3]);
-//   const float b0001 = Dot_prod(dx1, g0001[0], dy1, g0001[1],
-// 			       dz1, g0001[2], dt2, g0001[3]);
-//   const float b0010 = Dot_prod(dx1, g0010[0], dy1, g0010[1],
-// 			       dz2, g0010[2], dt1, g0010[3]);
-//   const float b0011 = Dot_prod(dx1, g0011[0], dy1, g0011[1],
-// 			       dz2, g0011[2], dt2, g0011[3]);
-//   const float b0100 = Dot_prod(dx1, g0100[0], dy2, g0100[1],
-// 			       dz1, g0100[2], dt1, g0100[3]);
-//   const float b0101 = Dot_prod(dx1, g0101[0], dy2, g0101[1],
-// 			       dz1, g0101[2], dt2, g0101[3]);
-//   const float b0110 = Dot_prod(dx1, g0110[0], dy2, g0110[1],
-// 			       dz2, g0110[2], dt1, g0110[3]);
-//   const float b0111 = Dot_prod(dx1, g0111[0], dy2, g0111[1],
-// 			       dz2, g0111[2], dt2, g0111[3]);
-//   const float b1000 = Dot_prod(dx2, g1000[0], dy1, g1000[1],
-// 			       dz1, g1000[2], dt1, g1000[3]);
-//   const float b1001 = Dot_prod(dx2, g1001[0], dy1, g1001[1],
-// 			       dz1, g1001[2], dt2, g1001[3]);
-//   const float b1010 = Dot_prod(dx2, g1010[0], dy1, g1010[1],
-// 			       dz2, g1010[2], dt1, g1010[3]);
-//   const float b1011 = Dot_prod(dx2, g1011[0], dy1, g1011[1],
-// 			       dz2, g1011[2], dt2, g1011[3]);
-//   const float b1100 = Dot_prod(dx2, g1100[0], dy2, g1100[1],
-// 			       dz1, g1100[2], dt1, g1100[3]);
-//   const float b1101 = Dot_prod(dx2, g1101[0], dy2, g1101[1],
-// 			       dz1, g1101[2], dt2, g1101[3]);
-//   const float b1110 = Dot_prod(dx2, g1110[0], dy2, g1110[1],
-// 			       dz2, g1110[2], dt1, g1110[3]);
-//   const float b1111 = Dot_prod(dx2, g1111[0], dy2, g1111[1],
-// 			       dz2, g1111[2], dt2, g1111[3]);
-
-//   /* Then the interpolations, down to the result */
-//   const float idx1 = Spline5 (dx1);
-//   const float idy1 = Spline5 (dy1);
-//   const float idz1 = Spline5 (dz1);
-//   const float idt1 = Spline5 (dt1);
-
-//   const float b111 = Linear (b1110, b1111, idt1);
-//   const float b110 = Linear (b1100, b1101, idt1);
-//   const float b101 = Linear (b1010, b1011, idt1);
-//   const float b100 = Linear (b1000, b1001, idt1);
-//   const float b011 = Linear (b0110, b0111, idt1);
-//   const float b010 = Linear (b0100, b0101, idt1);
-//   const float b001 = Linear (b0010, b0011, idt1);
-//   const float b000 = Linear (b0000, b0001, idt1);
-
-//   const float b11 = Linear (b110, b111, idz1);
-//   const float b10 = Linear (b100, b101, idz1);
-//   const float b01 = Linear (b010, b011, idz1);
-//   const float b00 = Linear (b000, b001, idz1);
-
-//   const float b1 = Linear (b10, b11, idy1);
-//   const float b0 = Linear (b00, b01, idy1);
-
-//   return Linear (b0, b1, idx1);
-// }
-
-
-/*
-** Function to load a Jpeg file.
-*/
-// int load_texture (const char * filename,
-// 			      unsigned char * dest,
-// 			      const int format,
-// 			      const unsigned int size)
-// {
-//   FILE *fd;
-//   struct jpeg_decompress_struct cinfo;
-//   struct jpeg_error_mgr jerr;
-//   unsigned char * line;
-
-//   cinfo.err = jpeg_std_error (&jerr);
-//   jpeg_create_decompress (&cinfo);
-
-//   if (0 == (fd = fopen(filename, "rb")))
-//     return 1;
-
-//   jpeg_stdio_src (&cinfo, fd);
-//   jpeg_read_header (&cinfo, TRUE);
-//   if ((cinfo.image_width != size) || (cinfo.image_height != size))
-//     return 1;
-
-//   if (GL_RGB == format)
-//     {
-//       if (cinfo.out_color_space == JCS_GRAYSCALE)
-// 	return 1;
-//     }
-//   else
-//     if (cinfo.out_color_space != JCS_GRAYSCALE)
-//       return 1;
-
-//   jpeg_start_decompress (&cinfo);
-
-//   while (cinfo.output_scanline < cinfo.output_height)
-//     {
-//       line = dest +
-// 	(GL_RGB == format ? 3 * size : size) * cinfo.output_scanline;
-//       jpeg_read_scanlines (&cinfo, &line, 1);
-//     }
-//   jpeg_finish_decompress (&cinfo);
-//   jpeg_destroy_decompress (&cinfo);
-//   return 0;
-// }
-
-// static float	z (const float x, const float y, const float t)
-// {
-//   const float x2 = x - 3;
-//   const float y2 = y + 1;
-//   const float xx = x2 * x2;
-//   const float yy = y2 * y2;
-//   return ((2 * sinf (20 * sqrtf (xx + yy) - 4 * t) +
-// 	   Noise (10 * x, 10 * y, t, 0)) / 200);
-// }
-
-
 
 #define MS_PER_CYCLE 20000
 // function prototypes:
@@ -625,6 +367,7 @@ void Animate()
     // Time = (float)ms / (float)MS_PER_CYCLE; // [0.,1.)
 
   /* Vertices */
+  if (!Frozen) {
   for (j = 0; j < RESOLUTION; j++)
     {
       y = (j + 1) * delta - 1;
@@ -753,7 +496,7 @@ void Animate()
 		normal[indice + 2] = 0;
 	      }
 	  }
-      }
+      }}
     // force a call to Display( ) next time it is convenient:
 
     glutSetWindow(MainWindow);
@@ -1299,7 +1042,10 @@ void Keyboard(unsigned char c, int x, int y)
     case 'O':
         WhichProjection = ORTHO;
         break;
-
+    case 'f':
+    case 'F':
+        Frozen  = !Frozen;
+        break;
     case 'p':
     case 'P':
         WhichProjection = PERSP;
