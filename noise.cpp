b640757d26ba5d10ff5bf874ed503c272f108cc1
Margaux <margaux.masson21@gmail.com>
2018-11-30 14:58:29 -0800

    [WIP] Animation water implemented

--- noise.cpp
+++ noise.cpp
@@ -18,6 +18,7 @@ static const char	gradient[32][4] =
 
 void InitNoise (void)
 {
+  printf("hellooooooooooo");
   unsigned int i = 0;
   while (i < 256)
     permut[i++] = rand () & MOD;
--- water_simulation.cpp
+++ water_simulation.cpp
@@ -360,7 +360,7 @@ int main(int argc, char *argv[])
 
 void Animate()
 {
-    InitNoise ();
+    // InitNoise ();
     // put animation stuff in here -- change some global variables
     // for Display( ) to find:
     // int ms = glutGet(GLUT_ELAPSED_TIME);
@@ -368,7 +368,194 @@ void Animate()
     // Time = (float)ms / (float)MS_PER_CYCLE; // [0.,1.)
 
   /* Vertices */
-  if (!Frozen) {
+//   printf("hello %d", permut[0]);
+//   if (!Frozen) {
+//   for (j = 0; j < RESOLUTION; j++)
+//     {
+//       y = (j + 1) * delta - 1;
+//       for (i = 0; i <= RESOLUTION; i++)
+// 	{
+// 	  indice = 6 * (i + j * (RESOLUTION + 1));
+
+// 	  x = i * delta - 1;
+// 	  surface[indice + 3] = x;
+// 	  surface[indice + 4] = z (x, y, t);
+// 	  surface[indice + 5] = y;
+// 	  if (j != 0)
+// 	    {
+// 	      /* Values were computed during the previous loop */
+// 	      preindice = 6 * (i + (j - 1) * (RESOLUTION + 1));
+// 	      surface[indice] = surface[preindice + 3];
+// 	      surface[indice + 1] = surface[preindice + 4];
+// 	      surface[indice + 2] = surface[preindice + 5];
+// 	    }
+// 	  else
+// 	    {
+// 	      surface[indice] = x;
+// 	      surface[indice + 1] = z (x, -1, t);
+// 	      surface[indice + 2] = -1;
+// 	    }
+// 	}
+//     }
+
+//   /* Normals */
+//   for (j = 0; j < RESOLUTION; j++)
+//     for (i = 0; i <= RESOLUTION; i++)
+//       {
+// 	indice = 6 * (i + j * (RESOLUTION + 1));
+
+// 	v1x = surface[indice + 3];
+// 	v1y = surface[indice + 4];
+// 	v1z = surface[indice + 5];
+
+// 	v2x = v1x;
+// 	v2y = surface[indice + 1];
+// 	v2z = surface[indice + 2];
+
+// 	if (i < RESOLUTION)
+// 	  {
+// 	    v3x = surface[indice + 9];
+// 	    v3y = surface[indice + 10];
+// 	    v3z = v1z;
+// 	  }
+// 	else
+// 	  {
+// 	    v3x = xn;
+// 	    v3y = z (xn, v1z, t);
+// 	    v3z = v1z;
+// 	  }
+
+// 	vax =  v2x - v1x;
+// 	vay =  v2y - v1y;
+// 	vaz =  v2z - v1z;
+
+// 	vbx = v3x - v1x;
+// 	vby = v3y - v1y;
+// 	vbz = v3z - v1z;
+
+// 	nx = (vby * vaz) - (vbz * vay);
+// 	ny = (vbz * vax) - (vbx * vaz);
+// 	nz = (vbx * vay) - (vby * vax);
+
+// 	l = sqrtf (nx * nx + ny * ny + nz * nz);
+// 	if (l != 0)
+// 	  {
+// 	    l = 1 / l;
+// 	    normal[indice + 3] = nx * l;
+// 	    normal[indice + 4] = ny * l;
+// 	    normal[indice + 5] = nz * l;
+// 	  }
+// 	else
+// 	  {
+// 	    normal[indice + 3] = 0;
+// 	    normal[indice + 4] = 1;
+// 	    normal[indice + 5] = 0;
+// 	  }
+
+
+// 	if (j != 0)
+// 	  {
+// 	    /* Values were computed during the previous loop */
+// 	    preindice = 6 * (i + (j - 1) * (RESOLUTION + 1));
+// 	    normal[indice] = normal[preindice + 3];
+// 	    normal[indice + 1] = normal[preindice + 4];
+// 	    normal[indice + 2] = normal[preindice + 5];
+// 	  }
+// 	else
+// 	  {
+// /* 	    v1x = v1x; */
+// 	    v1y = z (v1x, (j - 1) * delta - 1, t);
+// 	    v1z = (j - 1) * delta - 1;
+
+// /* 	    v3x = v3x; */
+// 	    v3y = z (v3x, v2z, t);
+// 	    v3z = v2z;
+
+// 	    vax = v1x - v2x;
+// 	    vay = v1y - v2y;
+// 	    vaz = v1z - v2z;
+
+// 	    vbx = v3x - v2x;
+// 	    vby = v3y - v2y;
+// 	    vbz = v3z - v2z;
+
+// 	    nx = (vby * vaz) - (vbz * vay);
+// 	    ny = (vbz * vax) - (vbx * vaz);
+// 	    nz = (vbx * vay) - (vby * vax);
+
+// 	    l = sqrtf (nx * nx + ny * ny + nz * nz);
+// 	    if (l != 0)
+// 	      {
+// 		l = 1 / l;
+// 		normal[indice] = nx * l;
+// 		normal[indice + 1] = ny * l;
+// 		normal[indice + 2] = nz * l;
+// 	      }
+// 	    else
+// 	      {
+// 		normal[indice] = 0;
+// 		normal[indice + 1] = 1;
+// 		normal[indice + 2] = 0;
+// 	      }
+// 	  }
+//       }}
+//     // force a call to Display( ) next time it is convenient:
+
+    glutSetWindow(MainWindow);
+    glutPostRedisplay();
+}
+
+// draw the complete scene:
+
+void Display()
+{
+      const float t = glutGet (GLUT_ELAPSED_TIME) / 1000.;
+  const float delta = 2. / RESOLUTION;
+  const unsigned int length = 2 * (RESOLUTION + 1);
+  const float xn = (RESOLUTION + 1) * delta + 1;
+  unsigned int i;
+  unsigned int j;
+  float x;
+  float y;
+  unsigned int indice;
+  unsigned int preindice;
+
+  /* Yes, I know, this is quite ugly... */
+  float v1x;
+  float v1y;
+  float v1z;
+
+  float v2x;
+  float v2y;
+  float v2z;
+
+  float v3x;
+  float v3y;
+  float v3z;
+
+  float vax;
+  float vay;
+  float vaz;
+
+  float vbx;
+  float vby;
+  float vbz;
+
+  float nx;
+  float ny;
+  float nz;
+
+  float l;
+
+
+  glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glLoadIdentity ();
+  glTranslatef (0, 0, -translate_z);
+  glRotatef (rotate_y, 1, 0, 0);
+  glRotatef (rotate_x, 0, 1, 0);
+
+  /* Vertices */
   for (j = 0; j < RESOLUTION; j++)
     {
       y = (j + 1) * delta - 1;
@@ -497,46 +684,87 @@ void Animate()
 		normal[indice + 2] = 0;
 	      }
 	  }
-      }}
-    // force a call to Display( ) next time it is convenient:
+      }
 
-    glutSetWindow(MainWindow);
-    glutPostRedisplay();
-}
+  /* The ground */
+  glPolygonMode (GL_FRONT_AND_BACK, GL_FILL);
+  glDisable (GL_TEXTURE_2D);
+  glColor3f (1, 0.9, 0.7);
+  glBegin (GL_TRIANGLE_FAN);
+  glVertex3f (-1, 0, -1);
+  glVertex3f (-1, 0,  1);
+  glVertex3f ( 1, 0,  1);
+  glVertex3f ( 1, 0, -1);
+  glEnd ();
 
-// draw the complete scene:
+  glTranslatef (0, 0.2, 0);
 
-void Display()
-{
+  /* Render wireframe? */
+  if (wire_frame != 0)
+    glPolygonMode (GL_FRONT_AND_BACK, GL_LINE);
+
+  /* The water */
+  glEnable (GL_TEXTURE_2D);
+  glColor3f (1, 1, 1);
+  glEnableClientState (GL_NORMAL_ARRAY);
+  glEnableClientState (GL_VERTEX_ARRAY);
+  glNormalPointer (GL_FLOAT, 0, normal);
+  glVertexPointer (3, GL_FLOAT, 0, surface);
+  for (i = 0; i < RESOLUTION; i++)
+    glDrawArrays (GL_TRIANGLE_STRIP, i * length, length);
+
+  /* Draw normals? */
+  if (normals != 0)
+    {
+      glDisable (GL_TEXTURE_2D);
+      glColor3f (1, 0, 0);
+      glBegin (GL_LINES);
+      for (j = 0; j < RESOLUTION; j++)
+	for (i = 0; i <= RESOLUTION; i++)
+	  {
+	    indice = 6 * (i + j * (RESOLUTION + 1));
+	    glVertex3fv (&(surface[indice]));
+	    glVertex3f (surface[indice] + normal[indice] / 50,
+			surface[indice + 1] + normal[indice + 1] / 50,
+			surface[indice + 2] + normal[indice + 2] / 50);
+	  }
+
+      glEnd ();
+    }
+
+  /* End */
+  glFlush ();
+//   glutSwapBuffers ();
+  glutPostRedisplay ();
     // InitNoise ();
-    unsigned char total_texture[4 * 256 * 256];
-    unsigned char alpha_texture[256 * 256];
-    unsigned char caustic_texture[3 * 256 * 256];
-    // unsigned int i;
+    // unsigned char total_texture[4 * 256 * 256];
+    // unsigned char alpha_texture[256 * 256];
+    // unsigned char caustic_texture[3 * 256 * 256];
+    // // unsigned int i;
 
-    /* Texture loading  */
-    glGenTextures (1, &texture);
-    load_texture ("alpha.jpg", alpha_texture, GL_ALPHA, 256);
-    load_texture ("reflection.jpg", caustic_texture, GL_RGB, 256);
+    // /* Texture loading  */
+    // glGenTextures (1, &texture);
+    // load_texture ("alpha.jpg", alpha_texture, GL_ALPHA, 256);
+    // load_texture ("reflection.jpg", caustic_texture, GL_RGB, 256);
 
-    for (int i = 0; i < 256 * 256; i++)
-        {
-        total_texture[4 * i] = caustic_texture[3 * i];
-        total_texture[4 * i + 1] = caustic_texture[3 * i + 1];
-        total_texture[4 * i + 2] = caustic_texture[3 * i + 2];
-        total_texture[4 * i + 3] = alpha_texture[i];
-        }
-    glBindTexture (GL_TEXTURE_2D, texture);
-    gluBuild2DMipmaps (GL_TEXTURE_2D, GL_RGBA, 256, 256, GL_RGBA,
-                GL_UNSIGNED_BYTE,  total_texture);
-    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-    glEnable (GL_TEXTURE_GEN_S);
-    glEnable (GL_TEXTURE_GEN_T);
-    glTexGeni (GL_S, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
-    glTexGeni (GL_T, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
+    // for (int i = 0; i < 256 * 256; i++)
+    //     {
+    //     total_texture[4 * i] = caustic_texture[3 * i];
+    //     total_texture[4 * i + 1] = caustic_texture[3 * i + 1];
+    //     total_texture[4 * i + 2] = caustic_texture[3 * i + 2];
+    //     total_texture[4 * i + 3] = alpha_texture[i];
+    //     }
+    // glBindTexture (GL_TEXTURE_2D, texture);
+    // gluBuild2DMipmaps (GL_TEXTURE_2D, GL_RGBA, 256, 256, GL_RGBA,
+    //             GL_UNSIGNED_BYTE,  total_texture);
+    // glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    // glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    // glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    // glEnable (GL_TEXTURE_GEN_S);
+    // glEnable (GL_TEXTURE_GEN_T);
+    // glTexGeni (GL_S, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
+    // glTexGeni (GL_T, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
 
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -636,6 +864,7 @@ void Display()
     glEnableClientState (GL_VERTEX_ARRAY);
     glNormalPointer (GL_FLOAT, 0, normal);
     glVertexPointer (3, GL_FLOAT, 0, surface);
+    printf("SURFAAAAAAAAAAACE: %d", surface[2]);
     for (int i = 0; i < 64; i++)
         glDrawArrays (GL_TRIANGLE_STRIP, i * length, length);
 
@@ -982,6 +1211,36 @@ void InitGraphics()
     glutMenuStateFunc(NULL);
     glutTimerFunc(-1, NULL, 0);
     glutIdleFunc(Animate);
+
+    InitNoise ();
+    unsigned char total_texture[4 * 256 * 256];
+    unsigned char alpha_texture[256 * 256];
+    unsigned char caustic_texture[3 * 256 * 256];
+    // unsigned int i;
+
+    /* Texture loading  */
+    glGenTextures (1, &texture);
+    load_texture ("alpha.jpg", alpha_texture, GL_ALPHA, 256);
+    load_texture ("reflection.jpg", caustic_texture, GL_RGB, 256);
+
+    for (int i = 0; i < 256 * 256; i++)
+        {
+        total_texture[4 * i] = caustic_texture[3 * i];
+        total_texture[4 * i + 1] = caustic_texture[3 * i + 1];
+        total_texture[4 * i + 2] = caustic_texture[3 * i + 2];
+        total_texture[4 * i + 3] = alpha_texture[i];
+        }
+    glBindTexture (GL_TEXTURE_2D, texture);
+    gluBuild2DMipmaps (GL_TEXTURE_2D, GL_RGBA, 256, 256, GL_RGBA,
+                GL_UNSIGNED_BYTE,  total_texture);
+    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glEnable (GL_TEXTURE_GEN_S);
+    glEnable (GL_TEXTURE_GEN_T);
+    glTexGeni (GL_S, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
+    glTexGeni (GL_T, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
 
     // init glew (a window must be open to do this):
 
