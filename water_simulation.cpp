6f38523009010c7c5511256955141a154116c41e
Margaux <margaux.masson21@gmail.com>
2018-11-30 18:23:00 -0800

    Clean up

--- water_simulation.cpp
+++ water_simulation.cpp
@@ -17,7 +17,7 @@
 #include <GL/glu.h>
 #include "glut.h"
 
-#include "bmptotexture.cpp"
+// #include "bmptotexture.cpp"
 #include "noise.cpp"
 float Time;
 #include "noise.h"
@@ -360,145 +360,6 @@ int main(int argc, char *argv[])
 
 void Animate()
 {
-    // InitNoise ();
-    // put animation stuff in here -- change some global variables
-    // for Display( ) to find:
-    // int ms = glutGet(GLUT_ELAPSED_TIME);
-    // ms %= MS_PER_CYCLE;
-    // Time = (float)ms / (float)MS_PER_CYCLE; // [0.,1.)
-
-    /* Vertices */
-    //   printf("hello %d", permut[0]);
-    //   if (!Frozen) {
-    //   for (j = 0; j < RESOLUTION; j++)
-    //     {
-    //       y = (j + 1) * delta - 1;
-    //       for (i = 0; i <= RESOLUTION; i++)
-    // 	{
-    // 	  indice = 6 * (i + j * (RESOLUTION + 1));
-
-    // 	  x = i * delta - 1;
-    // 	  surface[indice + 3] = x;
-    // 	  surface[indice + 4] = z (x, y, t);
-    // 	  surface[indice + 5] = y;
-    // 	  if (j != 0)
-    // 	    {
-    // 	      /* Values were computed during the previous loop */
-    // 	      preindice = 6 * (i + (j - 1) * (RESOLUTION + 1));
-    // 	      surface[indice] = surface[preindice + 3];
-    // 	      surface[indice + 1] = surface[preindice + 4];
-    // 	      surface[indice + 2] = surface[preindice + 5];
-    // 	    }
-    // 	  else
-    // 	    {
-    // 	      surface[indice] = x;
-    // 	      surface[indice + 1] = z (x, -1, t);
-    // 	      surface[indice + 2] = -1;
-    // 	    }
-    // 	}
-    //     }
-
-    //   /* Normals */
-    //   for (j = 0; j < RESOLUTION; j++)
-    //     for (i = 0; i <= RESOLUTION; i++)
-    //       {
-    // 	indice = 6 * (i + j * (RESOLUTION + 1));
-
-    // 	v1x = surface[indice + 3];
-    // 	v1y = surface[indice + 4];
-    // 	v1z = surface[indice + 5];
-
-    // 	v2x = v1x;
-    // 	v2y = surface[indice + 1];
-    // 	v2z = surface[indice + 2];
-
-    // 	if (i < RESOLUTION)
-    // 	  {
-    // 	    v3x = surface[indice + 9];
-    // 	    v3y = surface[indice + 10];
-    // 	    v3z = v1z;
-    // 	  }
-    // 	else
-    // 	  {
-    // 	    v3x = xn;
-    // 	    v3y = z (xn, v1z, t);
-    // 	    v3z = v1z;
-    // 	  }
-
-    // 	vax =  v2x - v1x;
-    // 	vay =  v2y - v1y;
-    // 	vaz =  v2z - v1z;
-
-    // 	vbx = v3x - v1x;
-    // 	vby = v3y - v1y;
-    // 	vbz = v3z - v1z;
-
-    // 	nx = (vby * vaz) - (vbz * vay);
-    // 	ny = (vbz * vax) - (vbx * vaz);
-    // 	nz = (vbx * vay) - (vby * vax);
-
-    // 	l = sqrtf (nx * nx + ny * ny + nz * nz);
-    // 	if (l != 0)
-    // 	  {
-    // 	    l = 1 / l;
-    // 	    normal[indice + 3] = nx * l;
-    // 	    normal[indice + 4] = ny * l;
-    // 	    normal[indice + 5] = nz * l;
-    // 	  }
-    // 	else
-    // 	  {
-    // 	    normal[indice + 3] = 0;
-    // 	    normal[indice + 4] = 1;
-    // 	    normal[indice + 5] = 0;
-    // 	  }
-
-    // 	if (j != 0)
-    // 	  {
-    // 	    /* Values were computed during the previous loop */
-    // 	    preindice = 6 * (i + (j - 1) * (RESOLUTION + 1));
-    // 	    normal[indice] = normal[preindice + 3];
-    // 	    normal[indice + 1] = normal[preindice + 4];
-    // 	    normal[indice + 2] = normal[preindice + 5];
-    // 	  }
-    // 	else
-    // 	  {
-    // /* 	    v1x = v1x; */
-    // 	    v1y = z (v1x, (j - 1) * delta - 1, t);
-    // 	    v1z = (j - 1) * delta - 1;
-
-    // /* 	    v3x = v3x; */
-    // 	    v3y = z (v3x, v2z, t);
-    // 	    v3z = v2z;
-
-    // 	    vax = v1x - v2x;
-    // 	    vay = v1y - v2y;
-    // 	    vaz = v1z - v2z;
-
-    // 	    vbx = v3x - v2x;
-    // 	    vby = v3y - v2y;
-    // 	    vbz = v3z - v2z;
-
-    // 	    nx = (vby * vaz) - (vbz * vay);
-    // 	    ny = (vbz * vax) - (vbx * vaz);
-    // 	    nz = (vbx * vay) - (vby * vax);
-
-    // 	    l = sqrtf (nx * nx + ny * ny + nz * nz);
-    // 	    if (l != 0)
-    // 	      {
-    // 		l = 1 / l;
-    // 		normal[indice] = nx * l;
-    // 		normal[indice + 1] = ny * l;
-    // 		normal[indice + 2] = nz * l;
-    // 	      }
-    // 	    else
-    // 	      {
-    // 		normal[indice] = 0;
-    // 		normal[indice + 1] = 1;
-    // 		normal[indice + 2] = 0;
-    // 	      }
-    // 	  }
-    //       }}
-    //     // force a call to Display( ) next time it is convenient:
 
     glutSetWindow(MainWindow);
     glutPostRedisplay();
@@ -519,7 +380,6 @@ void Display()
     unsigned int indice;
     unsigned int preindice;
 
-    /* Yes, I know, this is quite ugly... */
     float v1x;
     float v1y;
     float v1z;
@@ -683,19 +543,6 @@ void Display()
             }
         }
 
-    /* The ground */
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    glDisable(GL_TEXTURE_2D);
-    glColor3f(1, 0.9, 0.7);
-    glBegin(GL_TRIANGLE_FAN);
-    glVertex3f(-1, 0, -1);
-    glVertex3f(-1, 0, 1);
-    glVertex3f(1, 0, 1);
-    glVertex3f(1, 0, -1);
-    glEnd();
-
-    glTranslatef(0, 0.2, 0);
-
     for (int i = 0; i < 256 * 256; i++)
     {
         total_texture[4 * i] = Texture[3 * i];
@@ -715,52 +562,31 @@ void Display()
     glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
     glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
 
+    /* Draw normals */
+    // if (normals != 0)
+    // {
+    //     glDisable(GL_TEXTURE_2D);
+    //     glColor3f(1, 0, 0);
+    //     glBegin(GL_LINES);
+    //     for (j = 0; j < RESOLUTION; j++)
+    //         for (i = 0; i <= RESOLUTION; i++)
+    //         {
+    //             indice = 6 * (i + j * (RESOLUTION + 1));
+    //             glVertex3fv(&(surface[indice]));
+    //             glVertex3f(surface[indice] + normal[indice] / 50,
+    //                        surface[indice + 1] + normal[indice + 1] / 50,
+    //                        surface[indice + 2] + normal[indice + 2] / 50);
+    //         }
 
+    //     glEnd();
+    // }
 
+    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    /* Render wireframe? */
-    if (wire_frame != 0)
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    /* The water */
-    glEnable(GL_TEXTURE_2D);
-    glColor3f(1, 1, 1);
-    glEnableClientState(GL_NORMAL_ARRAY);
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glNormalPointer(GL_FLOAT, 0, normal);
-    glVertexPointer(3, GL_FLOAT, 0, surface);
-    for (i = 0; i < RESOLUTION; i++)
-        glDrawArrays(GL_TRIANGLE_STRIP, i * length, length);
-
-    /* Draw normals? */
-    if (normals != 0)
-    {
-        glDisable(GL_TEXTURE_2D);
-        glColor3f(1, 0, 0);
-        glBegin(GL_LINES);
-        for (j = 0; j < RESOLUTION; j++)
-            for (i = 0; i <= RESOLUTION; i++)
-            {
-                indice = 6 * (i + j * (RESOLUTION + 1));
-                glVertex3fv(&(surface[indice]));
-                glVertex3f(surface[indice] + normal[indice] / 50,
-                           surface[indice + 1] + normal[indice + 1] / 50,
-                           surface[indice + 2] + normal[indice + 2] / 50);
-            }
-
-        glEnd();
-    }
-
-    /* End */
-    glFlush();
-    glutPostRedisplay();
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glLoadIdentity();
-    glTranslatef(0, 0, -translate_z);
-    glRotatef(rotate_y, 1, 0, 0);
-    glRotatef(rotate_x, 0, 1, 0);
+    // glLoadIdentity();
+    // glTranslatef(0, 0, -translate_z);
+    // glRotatef(rotate_y, 1, 0, 0);
+    // glRotatef(rotate_x, 0, 1, 0);
 
     if (DebugOn != 0)
     {
@@ -844,6 +670,7 @@ void Display()
 
     if (wire_frame != 0)
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glDisable(GL_TEXTURE_2D); // does not work
 
     /* Water */
     glEnable(GL_TEXTURE_2D);
@@ -864,7 +691,7 @@ void Display()
     glEnable(GL_NORMALIZE);
 
     // draw the current object:
-    glCallList(BoxList);
+    // glCallList(BoxList);
 
     if (DepthFightingOn != 0)
     {
